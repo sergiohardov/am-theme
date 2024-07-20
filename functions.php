b5ca4157ee64e7a5cbe218755423bf7a122064df
sergiohardov <sergiohardov@gmail.com>
2024-07-20 12:47:10 +0300

    Create theme class

--- functions.php
+++ functions.php
@@ -1,6 +1,39 @@
 <?php
 
-require_once get_template_directory() . '/vendor/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php';
+if (!class_exists('AMTheme')) {
+    final class AMTheme
+    {
+        function __construct()
+        {
+            $this->libs();
+            $this->defines();
+        }
+
+        private function libs()
+        {
+            $libs = [
+                'Autoloader' => get_template_directory() . '/vendor/autoload.php',
+                'TGM Plugin Activation' => get_template_directory() . '/vendor/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php'
+            ];
+
+            foreach ($libs as $name => $path) {
+                if (file_exists($path)) {
+                    require_once $path;
+                } else {
+                    wp_die('Composer: ' . $name . ' not found.');
+                }
+            }
+        }
+
+        private function defines()
+        {
+            define('AMTHEME_URI', get_template_directory_uri());
+            define('AMTHEME_PATH', get_template_directory());
+            define('AMTHEME_TEXTDOMAIN', 'am-theme');
+        }
+    }
+    new AMTheme();
+}
 
 add_action('tgmpa_register', function () {
     $plugins = array(
