d6790efba032f3672ee263fe04ec391506b4ea56
sergiohardov <sergiohardov@gmail.com>
2024-07-21 14:47:37 +0300

    Change layouts to twig

--- components/parts/site-footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="site-footer">
-    <div class="am-container">
-        <p class="site-footer__copyright"><?php echo str_replace('[date]', date('Y'), get_theme_mod(AMTHEME_FIELD_FOOTER_COPY)) ?></p>
-    </div>
-</footer>
